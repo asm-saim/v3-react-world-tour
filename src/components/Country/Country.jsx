e8a5ac92723fd1a26e849a4efcf76fcfde0abf00
asm-saim <saim777m@gmail.com>
2026-06-30 10:29:25 +0600

    four info displayed in UI from api

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -3,9 +3,12 @@ import React from 'react';
 const Country = ({ country }) => {
     console.log(country)
     return (
-        <div>
+        <div className='universal'>
             <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <h3>Name: {country.name.common}</h3>
+            <h4>Capital: {country.capital.capital[0]}</h4>
+            <h4>Region: {country.region.region}</h4>
+            <h4>Population: {country.population.population}</h4>
         </div>
     );
 };
