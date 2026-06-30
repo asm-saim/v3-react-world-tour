532150cb0c44eca170f447385cc858ad8f18ade5
asm-saim <saim777m@gmail.com>
2026-06-30 13:58:58 +0600

    conditonal rendeing and ternery operator applied on btn

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,16 +1,32 @@
 import React from 'react';
 import './Country.css'
+import { useState } from 'react';
 
-const handleCountry = () => {
-    console.log("Btn clicked")
-}
 
 const Country = ({ country }) => {
+
+    const [visited, setVisit] = useState(false)
+
+    const handleVisit = () => {
+        // rule:1
+        // if (visited) {
+        //     setVisit(true)
+        // }
+        // else {
+        //     setVisit(false)
+        // }
+
+        //rule:2
+        // visited ? setVisit(false) : setVisit(true)
+
+        //rule:3
+        setVisit(!visited)
+    }
     // console.log(country)
     return (
         <div className='country'>
-            <img src={country.flags.flags.png} alt={country.flags.flags.alt} 
-            className='flag'/>
+            <img src={country.flags.flags.png} alt={country.flags.flags.alt}
+                className='flag' />
             <h2>Name: {country.name.common}</h2>
             <h4>Capital: {country.capital.capital[0]}</h4>
             <h4>Region: {country.region.region}</h4>
@@ -18,7 +34,9 @@ const Country = ({ country }) => {
             <h4>Area: {country.area.area}
                 {country.area.area > 300000 ? " (Big Country)" : " (Small Country)"}
             </h4>
-            <button className='button' onClick={handleCountry}>Not Visited</button>
+            <button className='button' onClick={handleVisit}>
+                {visited ? "Visited" : "Not Visited"}
+            </button>
         </div>
     );
 };
